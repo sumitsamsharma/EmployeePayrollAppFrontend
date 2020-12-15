4f681c7997c26aeb62c5ec8e50d10d606b15455c
sumitsamsharma <sharmasumit94286@gmail.com>
2020-12-15 23:54:40 +0530

    Employee Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateEmployeeComponent } from './employee-component/create-employee/create-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"create",component:CreateEmployeeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateEmployeeComponent } from './employee-component/create-employee/create-employee.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import {MatFormFieldModule,MatFormFieldControl} from '@angular/material/form-field';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
